8ead569a763bd40cafe0527a83af364864781c8f
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-04-29 13:52:45 +0200

    feat: allow sending one invite per user

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 type props = {
     id: string,
@@ -8,16 +8,18 @@ type props = {
 };
 
 function User({id, username, invited, invite}: props) {
+    const [invitedState, setInvitedState] = useState(invited)
+
 
     function handleClick() {
         invite(username);
-        invited = true;
+        setInvitedState(!invitedState);
     };
 
     return (
         <div id={id} className='flex justify-normal items-center px-5 border-black border-b-[1px] hover:bg-gray-300 h-12 w-full '>
             {username}
-            {!invited ? <button onClick={handleClick} className='ml-auto'><img src='/images/addContact.png' alt='add contact image' className='size-8'/></button> : null}
+            {!invitedState ? <button onClick={handleClick} className='ml-auto'><img src='/images/addContact.png' alt='add contact image' className='size-8'/></button> : null}
         </div>
     )
 }
