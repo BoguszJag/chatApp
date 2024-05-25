ac68ae60c6ba725b3dbb80de93b3a8b5bdcc9c35
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-05-25 20:00:31 +0200

    modify chat placeholder

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ChatInfoBar from './ChatInfoBar'
+import ChatBottomBar from './ChatBottomBar'
+import ChatMsgField from './ChatMsgField'
 
 function Chat() {
   
   return (
-    <div>Chat</div>
+    <div className='flex flex-col justify-normal items-start border-r border-y border-gray-400'>
+      <ChatInfoBar />
+      <ChatMsgField />
+      <ChatBottomBar />
+    </div>
   )
 }
 
