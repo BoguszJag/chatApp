454cb9658939798ea087cd1198eaa64a4540fd7f
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-05-30 13:25:09 +0200

    pass contactID to chat; disable writting and sending messages when no chat is selected

--- client/src/components/ChatBottomBar.tsx
+++ client/src/components/ChatBottomBar.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import useChat from '../hooks/useChatContext'
 
 function ChatBottomBar() {
+  const {chat} = useChat();
+
   return (
     <div className='flex flex-row mt-auto justify-normal items-center w-full h-[39px]'>
-      <input className='w-5/6 h-10 px-5 border-y border-r border-gray-400 placholder-gray-400 bg-gray-950' type="text" placeholder='Type a message...'/>
-      <button className='w-1/6 h-10 border-y border-gray-400'>Send</button>
+      <input disabled={!chat ? true : false} className='w-5/6 h-10 px-5 border-y border-r border-gray-400 placholder-gray-400 bg-gray-950' type="text" placeholder='Type a message...'/>
+      <button disabled={!chat ? true : false} className='w-1/6 h-10 border-y border-gray-400'>Send</button>
     </div>
   )
 }
--- client/src/components/ChatMsgField.tsx
+++ client/src/components/ChatMsgField.tsx
@@ -7,7 +7,7 @@ function ChatMsgField() {
 
   return (
     <div className='h-full w-full overflow-y-auto'>
-      {chat ? chat.map(messages => {
+      {chat ? chat.messages.map(messages => {
         return (
           <Message key={messages.msg_id} msgID={messages.msg_id} msgDate={messages.date} msgText={messages.msg_text} />
         )
