8e3c1ef95bb8115ac097153cbcb7c0eafdbda8e0
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-06-17 14:54:05 +0200

    update contacts on message sent/received

--- client/src/context/ContactsChatsProvider.tsx
+++ client/src/context/ContactsChatsProvider.tsx
@@ -1,12 +1,14 @@
 import React, { createContext, useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuthContext';
 import { ContactsChats, ContactsChatsContextType } from '../@types/ContactsChatsContext';
+import useSocket from '../hooks/useSocketContext';
 
 const ContactsChatsContext = createContext<ContactsChatsContextType | null>(null);
 
 export const ContactsChatsContextProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
   const {auth} = useAuth();
   const [contactsChats, setContactsChats] = useState<ContactsChats | null>(null);
+  const {socket} = useSocket();
 
   async function getContactsChats() {
     if(auth) {
@@ -35,6 +37,15 @@ export const ContactsChatsContextProvider: React.FC<{children: React.ReactNode}>
       getContactsChats();
     }, [auth]);
   
+    useEffect(() => {
+      
+      socket.on('updateChat', getContactsChats);
+      
+      return () => {
+        socket.off('updateChat', getContactsChats);
+      };
+
+    },[socket, contactsChats]);
 
   return (
     <ContactsChatsContext.Provider value={{ getContactsChats, contactsChats, setContactsChats }}>
