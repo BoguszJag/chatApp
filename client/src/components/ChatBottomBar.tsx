7c1fb672029340448b6495ace941d6f50e1512de
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-06-17 14:43:56 +0200

    change date format

--- client/src/components/ChatBottomBar.tsx
+++ client/src/components/ChatBottomBar.tsx
@@ -25,7 +25,7 @@ function ChatBottomBar() {
 
   const sendMessage = () => {
     const date = new Date();
-    const currentDate = `${('0'+date.getHours()).slice(-2)}:${('0'+date.getMinutes()).slice(-2)} ${('0'+date.getDate()).slice(-2)}.${(('0'+(date.getMonth()+1))).slice(-2)}.${date.getFullYear()}`;
+    const currentDate = `${('0'+date.getHours()).slice(-2)}:${('0'+date.getMinutes()).slice(-2)} ${date.getFullYear()}-${(('0'+(date.getMonth()+1))).slice(-2)}-${('0'+date.getDate()).slice(-2)}`;
     const text = input;
     setInput('');
 
