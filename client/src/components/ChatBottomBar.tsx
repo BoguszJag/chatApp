41cc071d502ff7566940ccd91b023c3799c9cc23
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-06-17 12:52:48 +0200

    change hover effect

--- client/src/components/ChatBottomBar.tsx
+++ client/src/components/ChatBottomBar.tsx
@@ -44,7 +44,7 @@ function ChatBottomBar() {
   return (
     <div className='flex flex-row mt-auto justify-normal items-center w-full h-[39px]'>
       <input disabled={!chat ? true : false} className='w-5/6 h-10 px-5 border-y border-r border-gray-400 placholder-gray-400 bg-gray-950' type="text" value={input} onChange={(e) => handleInput(e)} placeholder='Type a message...'/>
-      <button disabled={!chat ? true : false} className='w-1/6 h-10 border-y border-gray-400 hover:bg-gray-500 hover:text-black' onClick={sendMessage}>Send</button>
+      <button disabled={!chat ? true : false} className={'w-1/6 h-10 border-y border-gray-400 ' + (!chat ? null : 'hover:bg-gray-500 hover:text-black')} onClick={sendMessage}>Send</button>
     </div>
   )
 }
