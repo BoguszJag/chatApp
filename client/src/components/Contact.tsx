82be208d9f6b700e349d52793a67d56ec15b28f5
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-06-26 14:06:38 +0200

    add isDisplayed property to contact; visual notification on new unseen messages

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -4,16 +4,16 @@ type props = {
     id: string,
     username: string,
     lastMessage: string,
-    senderID: string
+    senderID: string,
+    isDisplayed: boolean,
     handleChat: (contactID: string, contactName: string) => void
 };
 
-function Contact({id, username, lastMessage, senderID, handleChat}: props) {
-
+function Contact({id, username, lastMessage, senderID, isDisplayed, handleChat}: props) {
     return (
         <div id={id} onClick={() => handleChat(id, username)} className='flex flex-col justify-normal px-5 border-gray-400 border-b-[1px] hover:bg-gray-500 hover:text-black hover:border-black h-12 w-full'>
             {username} 
-            <p className='text-[14px]'>{senderID ? (senderID !== id ? 'You: ' : username + ': ') : null}{lastMessage ? lastMessage.slice(0, 15) : null}</p>
+            <p className={`text-[14px]` + (!isDisplayed ? 'font-bold text-red-800' : null)}>{senderID ? (senderID !== id ? 'You: ' : username + ': ') : null}{lastMessage ? lastMessage.slice(0, 15) : null}</p>
         </div>
     )
 }
