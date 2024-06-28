2dd067e735d7dee5586e3fa687c3b9002d9c0c35
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-06-28 13:46:54 +0200

    change contacts type; fix searching when nothing is being typed; use contacts chats from context for searching insted of making a request

--- client/src/components/Contacts.tsx
+++ client/src/components/Contacts.tsx
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import useInvitations from '../hooks/useInvitationsContext';
 import SearchContacts from './SearchContacts';
 import Contact from './Contact';
 import useContactsChats from '../hooks/useContactsChatsContext';
 import useChat from '../hooks/useChatContext';
+import { ContactsChat } from '../@types/ContactsChatsContext';
 
 function Contacts() {
-  const [contacts, setContacts] = useState<[] | null>(null);
-  const {getInvites} = useInvitations(); 
+  const [contacts, setContacts] = useState<ContactsChat[] | null>(null);
   const [inputChange, setInputChange] = useState<string>('');
   const {getChat} = useChat();
   const {contactsChats} = useContactsChats();
 
-  async function handleInvites() {
-    await getInvites();
-  };
-
-  useEffect(() => {
-    handleInvites();
-  },[inputChange]);
-
   return (
     <div className='w-full'>
-        <SearchContacts handleInputChange={setInputChange} handleUsers={setContacts} apiRoute='/api/searchContacts'/>
+        <SearchContacts handleInputChange={setInputChange} handleUsers={setContacts} apiRoute={null}/>
         <div className='overflow-y-auto'>
-          {contacts && contacts.map(contact => {
+          {contacts && inputChange.length > 0 && contacts.map(contact => {
             return <Contact key={contact['id']} id={contact['id']} username={contact['username']} lastMessage={contact['last_msg']} senderID={contact['sender_id']} isDisplayed={contact['displayed']} handleChat={getChat}/>
           })}
         </div>
-        {contactsChats && inputChange.length === 0 ? contactsChats.sort((a,b) => {return new Date(b.msg_date).getTime() - new Date(a.msg_date).getTime()}).map(contact => {
+        {contactsChats && contactsChats.length > 0 && inputChange.length === 0 ? contactsChats.sort((a,b) => {return new Date(b.msg_date).getTime() - new Date(a.msg_date).getTime()}).map(contact => {
           return <Contact key={contact['id']} id={contact['id']} username={contact['username']} lastMessage={contact['last_msg']} senderID={contact['sender_id']} isDisplayed={contact['displayed']} handleChat={getChat}/>}) : null }
     </div>
   )
