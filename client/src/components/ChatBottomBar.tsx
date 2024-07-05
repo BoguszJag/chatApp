f6e0d30595d6b369691103fc46e1416cfc217ec8
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-05 13:53:19 +0200

    fix overflow on lower resolutions

--- client/src/components/ChatBottomBar.tsx
+++ client/src/components/ChatBottomBar.tsx
@@ -65,7 +65,7 @@ function ChatBottomBar() {
   return (
     <div ref={divRef} className='flex flex-row mt-auto justify-normal items-center w-full h-[39px] rounded-3xl'>
       <input disabled={!chat ? true : false} className='w-5/6 h-10 px-5 placholder-gray-400 bg-[#282b30] focus:outline-none rounded-bl-3xl' type="text" value={input} onChange={(e) => handleInput(e)} placeholder='Type a message...'/>
-      <button disabled={!chat ? true : false} className={'w-1/6 h-10 border-l border-[#2f3136] rounded-br-3xl bg-[#282b30] ' + (!chat ? null : 'hover:bg-[#40444b] hover:rounded-br-3xl')} onClick={sendMessage}>Send</button>
+      <button disabled={!chat ? true : false} className={'w-1/6 h-10 border-l border-[#2f3136] rounded-br-3xl bg-[#282b30] overflow-hidden ' + (!chat ? null : 'hover:bg-[#40444b] hover:rounded-br-3xl')} onClick={sendMessage}>Send</button>
     </div>
   )
 }
