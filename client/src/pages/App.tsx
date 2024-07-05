9e7bb56c1bb292880e6c40069404f4a2d3dda648
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-05 13:58:50 +0200

    track window width; create & display sidepanel button; styling changes for more responsive behaviour

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chat from '../components/Chat';
 import Sidepanel from '../components/Sidepanel';
+import SidePanelButton from '../components/SidePanelButton';
 
 
 function App() {
+  const [sidepanelState, setSidepanelState] = useState('hide');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+   
+  },[]);
 
   return (
-    <div className='grid grid-cols-chat bg-[#1e2124] text-gray-300 h-dvh overflow-hidden'>
-      <Sidepanel />
-      <Chat />
+    <div className={`relative grid bg-[#1e2124] text-gray-300 h-dvh overflow-hidden ` + (windowWidth >= 1024 ? 'grid-cols-chat' : 'sidepanel-' + sidepanelState)}>
+      {sidepanelState === 'hide' && windowWidth < 1024 ? null : <Sidepanel windowWidth={windowWidth}/>}
+      {windowWidth < 1024 ? <SidePanelButton currentSidepanelState={sidepanelState} handleSidepanelState={setSidepanelState}/> : null}
+      {sidepanelState === 'show' && windowWidth < 1024 ? null : <Chat windowWidth={windowWidth}/>}
     </div>
   );
 }
