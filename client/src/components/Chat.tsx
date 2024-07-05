62a423268e617e8bc9842a8d3631d8ef5b60e58d
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-05 13:55:11 +0200

    pass windowWidth to chat

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -3,10 +3,14 @@ import ChatInfoBar from './ChatInfoBar'
 import ChatBottomBar from './ChatBottomBar'
 import ChatMsgField from './ChatMsgField'
 
-function Chat() {
+type props = {
+  windowWidth: number
+};
+
+function Chat({windowWidth}: props) {
   
   return (
-    <div className='flex flex-col justify-normal items-start h-[98dvh] m-2 rounded-3xl bg-[#2f3136]'>
+    <div className={'flex flex-col justify-normal items-start h-[98dvh] m-2 rounded-3xl bg-[#2f3136]'}>
       <ChatInfoBar />
       <ChatMsgField />
       <ChatBottomBar />
