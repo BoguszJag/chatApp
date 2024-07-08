cea160bcdaed16ea2ede626ccc40646de4108a21
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-08 15:18:14 +0200

    styling changes

--- client/src/components/FormButton.tsx
+++ client/src/components/FormButton.tsx
@@ -8,7 +8,7 @@ type props = {
 
 function FormButton({buttonText, onClick, action}: props) {
     return (
-    <button className='w-2/3 shadow-md rounded-xl text-lg bg-gradient-to-r from-sky-500 to-green-200 hover:from-sky-600 hover:to-green-300 my-5' onClick={onClick} formAction={action} formMethod='POST'>{buttonText}</button>
+    <button className='w-1/3 shadow-md rounded-3xl text-lg p-[2px] mb-5 bg-[#40444b] hover:bg-gray-400 hover:text-[#1e2124]' onClick={onClick} formAction={action} formMethod='POST'>{buttonText}</button>
   )
 }
 
