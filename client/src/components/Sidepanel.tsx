9600d194bbf4b6ce030220dd347c96fedf4fbab4
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-08 15:21:16 +0200

    styling changes

--- client/src/components/Sidepanel.tsx
+++ client/src/components/Sidepanel.tsx
@@ -7,10 +7,11 @@ import useInvitations from '../hooks/useInvitationsContext';
 import useContactsChats from '../hooks/useContactsChatsContext';
 
 type props = {
-  windowWidth: number
+  windowWidth: number,
+  handleSidepanelState: React.Dispatch<React.SetStateAction<string>>
 };
 
-function Sidepanel({windowWidth}: props) {
+function Sidepanel({windowWidth, handleSidepanelState}: props) {
   const [switchView, setSwitchView] = useState({viewAddContacts: false, viewInvitations: false});
   const {getInvites} = useInvitations();
   const {getContactsChats} = useContactsChats();
@@ -50,12 +51,12 @@ function Sidepanel({windowWidth}: props) {
   }, [switchView.viewInvitations]);
 
   return (
-    <div className={'flex flex-col justfiy-normal items-start h-[98dvh] m-2 mr-0 rounded-3xl bg-[#2f3136] ' + (windowWidth < 1024 ? 'mr-2' : null)}>
+    <div className={'flex flex-col justfiy-normal items-start h-[98dvh] m-2 mr-0 rounded-3xl bg-[#2f3136] shadow-lg ' + (windowWidth < 1024 ? 'mr-2' : null)}>
         <div className='flex justify-normal items-start w-full'>
           <button className='w-1/2 h-10 border-r border-[#2f3136] bg-[#282b30] rounded-tl-3xl hover:bg-[#40444b] hover:rounded-tl-3xl overflow-hidden' onClick={handleContactsView}>{switchView.viewAddContacts ? 'Contacts' : 'Add Contacts'}</button>
           <button className='w-1/2 h-10 bg-[#282b30] rounded-tr-3xl hover:bg-[#40444b] hover:rounded-tr-3xl overflow-hidden' name='invitations' onClick={handleInvitationsView}>Invitations</button>  
         </div>
-        {switchView.viewInvitations ? <Invitations /> : switchView.viewAddContacts ? <AddContacts /> : <Contacts />}
+        {switchView.viewInvitations ? <Invitations /> : switchView.viewAddContacts ? <AddContacts /> : <Contacts handleSidepanelState={handleSidepanelState}/>}
         <LogoutButton />
     </div>
   )
