b77841c2b19f4b779cb2b9a1fc5e6e2b119ec12a
Bogusz <bogusz.jagiellowicz@gmail.com>
2024-07-12 13:05:13 +0200

    hide overflow

--- client/src/components/ChatInfoBar.tsx
+++ client/src/components/ChatInfoBar.tsx
@@ -5,7 +5,7 @@ function ChatInfoBar() {
   const {chat} = useChat();
 
   return (
-    <div className='flex flex-shrink-0 justify-normal items-center w-full h-[39px] rounded-t-3xl bg-[#282b30]'>
+    <div className='flex flex-shrink-0 justify-normal items-center w-full h-[39px] rounded-t-3xl bg-[#282b30] overflow-hidden'>
       <p className='ml-4'>{chat ? chat.contactName : null}</p>
     </div>
   )
