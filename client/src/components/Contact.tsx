cc16e5aa17fa62df5ec73cf78904a38e7fd8019d
Bogusz <bogusz.jagiellowicz@gmail.com>
2025-08-26 14:40:54 +0200

    change types and names of variables

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import useChat from '../hooks/useChatContext';
 
 type props = {
-    id: string,
+    uid: string,
     username: string,
-    lastMessage: string,
-    senderID: string,
-    isDisplayed: boolean,
+    lastMessage: string | null,
+    senderID: string | null,
+    isDisplayed: boolean | null,
     handleChat: (contactID: string, contactName: string) => void,
     handleSidepanelState: React.Dispatch<React.SetStateAction<string>>
 };
 
-function Contact({id, username, lastMessage, senderID, isDisplayed, handleChat, handleSidepanelState}: props) {
-    const {chat} = useChat();
+function Contact({uid, username, lastMessage, senderID, isDisplayed, handleChat, handleSidepanelState}: props) {
 
     const handleClick = () => {
-        handleChat(id, username);
+        handleChat(uid, username);
         handleSidepanelState('hide');
     };
 
+// onClick={uid !== chat?.contact ? () => handleClick() : undefined}
+
     return (
-        <div id={id} onClick={id !== chat?.contact ? () => handleClick() : undefined} className='flex flex-col justify-normal px-2 py-1 hover:bg-[#40444b] mx-3 rounded-xl overflow-hidden text-nowrap'>
+        <div id={uid} onClick={handleClick} className='flex flex-col justify-normal px-2 py-1 hover:bg-[#40444b] mx-3 rounded-xl overflow-hidden text-nowrap'>
             {<p className={!isDisplayed ? 'oswald-700 text-gray-400' : ''}>{username}</p>} 
-            <p className={`text-[14px] ` + (!isDisplayed ? 'oswald-700 text-gray-400' : null)}>{senderID ? (senderID !== id ? 'You: ' : username + ': ') : null}{lastMessage ? lastMessage.slice(0, 15) : null}</p>
+            <p className={`text-[14px] ` + (!isDisplayed ? 'oswald-700 text-gray-400' : null)}>{senderID ? (senderID !== uid ? 'You: ' : username + ': ') : null}{lastMessage ? lastMessage.slice(0, 15) : null}</p>
         </div>
     )
 }
